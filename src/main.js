01abbc5394ed1524f0f87f724ac807f9ba75b02e
Sok Masterly <sokmasterlychaon06@gmail.com>
2025-04-13 00:49:51 +0700

    deleted some files

--- src/main.js
+++ src/main.js
@@ -1,17 +1,12 @@
 import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia' // Like the brain of your app — stores your state (cart, user, etc.)
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate' // Persist store in localStorage/sessionStorage: keep data after refresh(Like memory 💾 — remembers your data even after refresh)
 import App from './App.vue'
 import router from './router'
-// Global UI Components Plugin
-import GlobalComponents from './plugins/global-components'
-
 
 const app = createApp(App)
-const pinia = createPinia()       
-app.use(pinia) 
+const pinia = createPinia()
+app.use(pinia)
 app.use(router)
-app.use(GlobalComponents)
-pinia.use(piniaPluginPersistedstate) // keep data after refreshing
+
 app.mount('#app')
